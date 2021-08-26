1ef6dcfe0c5a2c98458bc3b1125f08a493cefee5
Oliver Strickson <ostrickson@turing.ac.uk>
2021-08-26 10:57:36 +0100

    Column selection and matching

--- pace/missingness.py
+++ pace/missingness.py
@@ -1,9 +1,14 @@
 import pandas as pd
-from dataclasses import dataclass
-from typing import Sequence, Callable, Optional, Union, Any
+from typing import Sequence, Callable, Optional, Any
+from .setexpression import Set, SetExpr
+
+# alias for Set
+class Col(Set):
+    pass
 
 
 class Missingness(object):
+
     _pattern: pd.DataFrame
     _missingness: pd.DataFrame
 
@@ -23,20 +28,65 @@ class Missingness(object):
             self._check()
 
     def _check(self):
-        """
-        Check that the DataFrame 'missingness' has a column
+        """Check that the DataFrame 'missingness' has a column
         'missingness_pattern' with a foreign key relationship to
         'pattern'.
+
         """
         assert self._pattern_key in self._missingness.keys()
-        assert self._missingness[self._pattern_key].isin(self._pattern.index).all()
+        assert (
+            self._missingness[self._pattern_key]
+            .isin(self._pattern.index)
+            .all()
+        )
 
-    def select_columns(self, col_selection: Sequence):
+    def pattern_counts(
+        self,
+        count_col_name: str = "_count",
+        selection: Optional[Sequence] = None,
+    ):
+        """Return the missingness patterns of the data, along with the count
+        of each.
+
         """
-        Return a new Missingness object based on the column selection. 
-        Combine missingness patterns.
+        selection1 = selection or self._missingness.index
+
+        counts = pd.DataFrame(
+            self._missingness.loc[selection1]
+            .groupby(self._pattern_key)
+            .size(),
+            columns=[count_col_name],
+        )
+
+        return self._pattern.join(counts, how="right")
+
+    def _run_set(self, pattern_spec):
+        return pattern_spec.run_with(self._pattern.__getitem__)
+
+    def pattern_matches(
+        self, pattern_spec: SetExpr, row_selection: Optional[Sequence] = None,
+    ) -> pd.Series:
+        """Return a boolean series indicating where the data matches the given
+        missingness pattern (as pattern_spec)
+
         """
-        new_groups = self._pattern[col_selection].groupby(col_selection, as_index=False)
+        row_selection1 = row_selection or self._missingness.index
+        selected = self._missingness.loc[row_selection1]
+
+        return selected[self._pattern_key].isin(
+            self._pattern[self._run_set(pattern_spec)].index
+        )
+
+    def select_columns(self, col_selection) -> pd.DataFrame:
+        """Return a new Missingness object based on the given column selection
+        (which must be a subset of columns of the current object).
+        Patterns that are identical under the selection are
+        consolidated.
+
+        """
+        new_groups = self._pattern[col_selection].groupby(
+            col_selection, as_index=False
+        )
         new_pattern = new_groups.first()
         group_mapping = new_groups.ngroup().rename(self._pattern_key)
 
@@ -49,38 +99,13 @@ class Missingness(object):
 
         return Missingness(new_pattern, new_missingness)
 
-    def pattern_counts(
-        self, count_col_name: str = "_count", selection: Optional[Sequence] = None
-    ):
-        """
-        Return the missingness patterns of the data, along with the
-        count of each in the data
-        """
-        selection1 = (selection or self._missingness.index,)
-
-        counts = pd.DataFrame(
-            self._missingness.loc[selection1].groupby(self._pattern_key).size(),
-            columns=[count_col_name],
-        )
-
-        return self._pattern.join(counts, how="right")
-
-    # def pattern_counts_alt(self, col_name: str = "_count"):
-    #     """
-    #     Alternative implementation of pattern_counts for performance comparison
-    #     """
-    #     groups = self._missingness.groupby(self._pattern_key).groups
-    #     return self._pattern.join(
-    #         pd.Series((len(v) for v in groups.values()), name=col_name)
-    #     )
-
     @classmethod
     def from_data_frame(
         cls,
         df: pd.DataFrame,
         pattern_key: str = "pattern_key",
         is_missing: Callable[[Any], bool] = pd.isnull,
-    ):
+    ) -> Missingness:
         grouped = is_missing(df).groupby(list(df))
         return cls(
             missingness=pd.DataFrame(grouped.ngroup(), columns=[pattern_key]),
@@ -89,18 +114,22 @@ class Missingness(object):
         )
 
     @classmethod
-    def from_csv(cls, filepath_or_buffer, *args, **kwargs):
+    def from_csv(cls, filepath_or_buffer, *args, **kwargs) -> Missingness:
         df = pd.read_csv(filepath_or_buffer, *args, **kwargs)
         return cls.from_data_frame(df)
 
     @classmethod
-    def from_postgres(cls, conn):
+    def from_postgres(cls, conn) -> Missingness:
         raise NotImplementedError()
 
 
 def heatmap_data(m: Missingness, count_col="_count"):
     counts = m.pattern_counts(count_col)
-    return counts.astype(int).mul(counts[count_col], axis=0).drop(count_col, axis=1)
+    return (
+        counts.astype(int)
+        .mul(counts[count_col], axis=0)
+        .drop(count_col, axis=1)
+    )
 
 
 ## TODO
