6d75bde40964634d6d0b8e9b2bdd4abe8e45f1f3
Oliver Strickson <ostrickson@turing.ac.uk>
2021-09-07 07:40:59 +0100

    Add test of matches; renaming

--- tests/test_missingness.py
+++ tests/test_missingness.py
@@ -1,9 +1,18 @@
 import pytest
 import pandas as pd
-from pace.missingness import Missingness
+from pace.missingness import Missingness, Col
 
 
-def test_missingness():
+def example_df():
+    return pd.DataFrame(
+        data={
+            "a": [1, pd.NA, pd.NA, 4, 2, 1, pd.NA],
+            "b": [1, pd.NA, 1, pd.NA, 1, pd.NA, 1],
+        }
+    )
+
+
+def test_missingness_construction():
     """Basic checks of constructing Missingness"""
 
     missingness1 = pd.DataFrame(data={"pattern_key": [0, 1, 1, 2]})
@@ -18,18 +27,13 @@ def test_missingness():
         Missingness(pattern=pattern2, missingness=missingness2)
 
 
-def test_missingness2():
+def test_missingness_from_df():
     """
     Check construction of Missingness from a pandas data frame, and
     that the original dataframe missingness can be reconstructed.
     """
 
-    df = pd.DataFrame(
-        data={
-            "a": [1, pd.NA, pd.NA, 4, 2, 1, pd.NA],
-            "b": [1, pd.NA, 1, pd.NA, 1, pd.NA, 1],
-        }
-    )
+    df = example_df()
 
     m = Missingness.from_data_frame(df)
     assert (
@@ -37,3 +41,16 @@ def test_missingness2():
         .drop("pattern_key", axis=1)
         .equals(df.isnull())
     )
+
+
+def test_missingness_match():
+    df = example_df()
+    m = Missingness.from_data_frame(df)
+
+    expected1 = pd.Series([False, True, True, False, False, False, True])
+
+    assert m.matches(Col("a")).equals(expected1)
+
+    expected2 = pd.Series([False, True, True, True, False, True, True])
+
+    assert m.matches(Col("a") | Col("b")).equals(expected2)
