7c813335687d6f7e90eee108641504fed4df2e4c
Oliver Strickson <ostrickson@turing.ac.uk>
2021-09-16 09:04:23 +0100

    Refactor PlotMissingData: use selection history, separate plots

--- pace/plots.py
+++ pace/plots.py
@@ -1,192 +1,249 @@
-from .datastructure import MissingData
+from pace.missingness import Missingness
+import pace.missingness as missingness
+from pace.history import SelectionHistory, Selection
+import bokeh.plotting
 from bokeh.plotting import figure, show
 from bokeh.models import ColumnDataSource, LinearColorMapper, tools, CustomJS
 from bokeh.palettes import Oranges256
 from bokeh.transform import transform, linear_cmap
 from bokeh.models.widgets import Panel, Tabs  # not sure if needed
-from bokeh.io import output_file, show  # not sure if needed
+import bokeh.io
 import pandas as pd
 import numpy as np
 import logging
-
 import base64
+from weakref import WeakValueDictionary
 from IPython.display import Javascript, display
 from ipywidgets import widgets
+from typing import Any, Sequence, Dict, Tuple
+from abc import ABC, abstractmethod
 
 # Set up logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-TOOLS = "box_select, tap, reset"
+
+class MissingnessPlotBase:
+    def __init__(self, data: Missingness):
+        raise NotImplementedError()
+
+    def plot(self, initial_selection: Selection) -> bokeh.plotting.Figure:
+        raise NotImplementedError()
+
+    def selection_to_plot_indices(self, selection: Selection) -> Sequence[int]:
+        raise NotImplementedError()
+
+    def plot_indices_to_selection(self, indices: Sequence[int]) -> Selection:
+        raise NotImplementedError()
 
 
-class PlotMissingData(MissingData):
-    """
-    Class for plotting missing data 
-    """
+class ValueBarChart(MissingnessPlotBase):
+    def __init__(self, data: Missingness):
+        self._data = data
 
-    def __init__(self, dataset):
-        super().__init__(dataset)
-        self.combinations_matrix = self.dict_to_matrix()
-        self.long_combinations_matrix = self.dict_to_long_matrix()
-
-    def dict_to_long_matrix(self):
-        """
-        Takes the dictionary of the missing_data instance that holds the
-        information about the missing combinations and converts it into
-        the matrix/column data source format required for the plots.
-
-        difference to dict_to_matrix: not full matrix
-        """
-        count = 0  # counts over number of combination
-        index = []
-        variable = []
-        value = []
-        for combination, rows in self.missing_combinations.items():
-            index += list(
-                np.ones((len(combination), 1), dtype=int)[:, 0] * count
-            )
-            variable += list(
-                combination
-            )  # should combinations be list instead of tuple all along?
-            value += list(
-                np.ones((len(combination), 1), dtype=int)[:, 0] * len(rows)
-            )
-            count += 1
-        zipped_list = list(zip(index, variable, value))
-        long_combination_matrix = pd.DataFrame(
-            zipped_list, columns=["index", "variable", "value"]
-        )
-        return long_combination_matrix
-
-    def dict_to_matrix(self):
-        """
-        Takes the dictionary of the missing_data instance that holds the
-        information about the missing combinations and converts it into
-        the matrix/column data source format required for the plots.
-        """
-        fields = list(
-            self.missing_fields["Field"][self.missing_fields["Field"] != 0]
-        )
-        # fields = list()
-        combinations_matrix = pd.DataFrame(
-            0, index=np.arange(len(self.missing_combinations)), columns=fields
+        self.source = ColumnDataSource(
+            missingness.value_bar_chart_data(data).reset_index()
         )
 
-        index = 0
-        for combination, rows in self.missing_combinations.items():
-            count = len(rows)
-            for field_name in combination:
-                combinations_matrix.loc[
-                    index, field_name
-                ] = count  # TODO: at instead count?
-            index += 1
+        self.bar_width = 0.5
+        self.tools = ["box_select", "tap", "reset"]
+        self.height = 960
+        self.width = 960
+        self.title = "Value bar chart"
+        self.ylabel = "Number of missing values"
 
-        return combinations_matrix
-
-    def value_bar_chart(self, selected_indices=[]):
-        """Plots the number of missing values for each field in the dataset as
-        a bar chart :param missing_data: MissingDataInstance
-
-        """
-        source = ColumnDataSource(self.missing_fields)
-        width = 0.5  # width of bars
-        p = figure(title="Value bar chart", tools=TOOLS, width=960, height=960)
-        p.vbar(x="Field", top="Count", width=width, source=source)
-        p.xaxis.ticker = list(self.missing_fields["Field"])
-        p.xaxis.major_label_overrides = self.lookup_table_backward
+    def plot(self, initial_selection=Selection()) -> bokeh.plotting.Figure:
+        p = figure(
+            title=self.title,
+            tools=self.tools,
+            width=self.width,
+            height=self.height,
+            x_range=self._data.column_labels(),
+        )
+        p.vbar(
+            x="index", top="_count", width=self.bar_width, source=self.source
+        )
         p.xaxis.major_label_orientation = "vertical"
-        p.yaxis.axis_label = "Number of missing values"
+        p.yaxis.axis_label = self.ylabel
 
-        # make a custom javascript callback that exports the indices of the selected points
-        # to the Jupyter notebook
-        callback = CustomJS(
-            args=dict(source=source),
-            code="""
-                                console.log('Running CustomJS callback now.');
-                                var indices = source.selected.indices;
-                                var data = source.selected.data;
-                                var kernel = IPython.notebook.kernel;
-                                kernel.execute("selected_indices = " + indices)
-                                """,
-        )
-
-        # set the callback to run when a selection geometry event occurs in the figure
-        p.js_on_event("selectiongeometry", callback)
-        selected_indices = list(source.data["index"])
         return p
 
-    def combination_heatmap(self, selected_indices=[]):
-        """Plots a heatmap with all unique combinations of missing fields
-        :param missing_data: MissingDataInstance
-
-        """
-        source = ColumnDataSource(self.long_combinations_matrix)
-
-        p = figure(
-            title="Combination heatmap",
-            width=960,
-            height=960,
-            tools=TOOLS,  #
-            y_range=[
-                str(x)
-                for x in np.arange(
-                    self.long_combinations_matrix["index"].max() + 1
-                )
-            ],
+    def plot_indices_to_selection(self, indices: Sequence[int]) -> Selection:
+        col_labels = self._data.column_labels()
+        return Selection(
+            columns=self._data.invert_column_selection(
+                [col_labels[i] for i in indices]
+            )
         )
 
-        p.background_fill_color = "#cccccc"
-        p.grid.visible = False
+    def selection_to_plot_indices(self):
+        pass
+
+
+class CombinationHeatmap(MissingnessPlotBase):
+    def __init__(self, data: Missingness):
+        self._data = data
+
+        heatmap_data = missingness.heatmap_data(data)
+        heatmap_data["pattern_key_str"] = heatmap_data.index.values.astype(str)
+        heatmap_data_long = pd.melt(
+            heatmap_data.reset_index(),
+            id_vars=["pattern_key", "pattern_key_str"],
+        )
+        self.source = ColumnDataSource(heatmap_data_long)
+
+        self.x_range = list(heatmap_data)
+        self.y_range = list(heatmap_data["pattern_key_str"].values)
+        self.c_max = heatmap_data_long["value"].max()
+
+        self.xlabel = "Fields"
+        self.ylabel = "Combinations"
+
+        self.width = 960
+        self.height = 960
+        self.tools = ["box_select", "tap", "reset"]
+        self.fill = "#cccccc"
+        self.grid_visible = False
+
+    def plot(self, initial_selection=None) -> bokeh.plotting.Figure:
+        p = figure(
+            title="Combination heatmap",
+            width=self.width,
+            height=self.height,
+            tools=self.tools,
+            x_range=self.x_range,
+            y_range=self.y_range,
+        )
+        p.background_fill_color = self.fill
+        p.grid.visible = self.grid_visible
 
         p.rect(
-            y="index",
+            y="pattern_key_str",
             x="variable",
-            source=source,
+            source=self.source,
             width=1.0,
             height=1.0,
             fill_color=linear_cmap(
                 field_name="value",
                 palette=list(reversed(Oranges256)),
                 low=1,
-                high=self.long_combinations_matrix["value"].max(),
+                high=self.c_max,
+                # transparent
                 low_color="#00000000",
             ),
-            line_color="#cccccc",
+            line_color=self.fill,
         )
         p.xaxis.major_label_orientation = "vertical"
-        p.yaxis.axis_label = "Combinations"
-        p.xaxis.axis_label = "Fields"
         p.yaxis.major_label_text_color = None
+        p.yaxis.axis_label = self.ylabel
+        p.xaxis.axis_label = self.xlabel
 
-        callback = CustomJS(
-            args=dict(source=source),
-            code="""
-                                console.log('Running CustomJS callback now.');
-                                var indices = source.selected.indices;
-                                var data = source.selected.data;
-                                var kernel = IPython.notebook.kernel;
-                                kernel.execute("selected_indices = " + indices)
-                                """,
-        )
-
-        # set the callback to run when a selection geometry event occurs in the figure
-        p.js_on_event("selectiongeometry", callback)
-        selected_indices = list(source.data["index"])
         return p
 
-    def plot_data(self, selected_indices=[]):
+    def plot_indices_to_selection(self, indices: Sequence[int]) -> Selection:
+        n_combinations = len(self._data.patterns())
+        pattern_key = self._data.patterns().index.values
+        include = list({pattern_key[i % n_combinations] for i in indices})
+        exclude = self._data.invert_pattern_selection(include)
+        return Selection(patterns=exclude)
+
+    def selection_to_plot_indices(self):
+        pass
+
+
+class PlotMissingData:
+    """Class for plotting missing data"""
+
+    _selection_history: SelectionHistory
+    _plots: Dict[Tuple[Any, Any], MissingnessPlotBase]
+
+    # A class-member dictionary holding PlotMissingData objects, keyed
+    # by their id().  This allows the JavaScript callback (that in
+    # turn runs code in the IPython kernel) to look up the caller. The
+    # Bokeh plot selection can then be passed to a method of the
+    # corresponding object.  Uses WeakValueDictionary so that the
+    # reference here does not prevent the object being garbage
+    # collected.
+    _instances = WeakValueDictionary()
+
+    def __init__(self, df):
+        bokeh.io.output_notebook(hide_banner=True)
+
+        m = Missingness.from_data_frame(df)
+
+        self._selection_history = SelectionHistory(m)
+        self._plots = {}
+
+        PlotMissingData._instances[id(self)] = self
+
+    def _selection_callback(self, source, name, tabname):
+        """Return a javascript callback that updates the particular
+        PlotMissingData instance according to the selection made in a
+        Bokeh plot.
+
+        """
+        return CustomJS(
+            args={
+                "source": source,
+                "instance_id": id(self),
+                "name": repr(name),
+                "tabname": repr(tabname),
+            },
+            code="""
+            var indices = source.selected.indices;
+            var kernel = IPython.notebook.kernel;
+            // ensure that pace.plots is imported under its canonical name
+            kernel.execute("import pace.plots");
+            kernel.execute(`(
+                pace.plots.PlotMissingData
+                ._instances[${instance_id}]
+                ._update_selection(
+                    ${name},
+                    ${tabname},
+                    [${indices}],
+                )
+            )`);
+            """,
+        )
+
+    def _update_selection(self, name, tabname, indices):
+        self._selection_history.update_active(
+            name,
+            self._plots[name, tabname].plot_indices_to_selection(indices),
+        )
+
+    def _add_subplot(self, plotter_cls, name, tabname):
+
+        m = self._selection_history.missingness(name)
+        plotter = plotter_cls(m)
+
+        self._plots[name, tabname] = plotter
+
+        callback = self._selection_callback(plotter.source, name, tabname)
+
+        p = plotter.plot()
+        p.js_on_event("selectiongeometry", callback)
+
+        return p
+
+    def add_plot(self, name, based_on=None):
         """Creates all the plot options and shows them in a tab layout"""
-        p1 = self.value_bar_chart(selected_indices=[])
+
+        self._selection_history.new_selection(name, based_on)
+
+        p1 = self._add_subplot(ValueBarChart, name, "value_bar_chart")
         tab1 = Panel(child=p1, title="Value bar chart")
-        p2 = self.combination_heatmap()
+
+        p2 = self._add_subplot(CombinationHeatmap, name, "combination_heatmap")
         tab2 = Panel(child=p2, title="Combination heatmap")
-        tabs = Tabs(tabs=[tab1, tab2])
+
+        tabs = Tabs(tabs=[tab1, tab2], active=0)
 
         show(tabs)
+
         logging.info(
             f"""
-            
+
     # ********
     # To add a plot, insert a new cell below, type "add_plot(selected_indices)" and run cell.
     # ********"""
