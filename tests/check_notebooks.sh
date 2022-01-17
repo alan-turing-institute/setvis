581692826cb826a4609eb25f7f91db6cfa23746b
Oliver Strickson <ostrickson@turing.ac.uk>
2022-01-17 14:48:08 +0000

    Add script to check notebooks

---
+++ tests/check_notebooks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Runs the notebooks in 'notebooks', apart from the indicated
+# exceptions, but does not check their output (only that they ran
+# successfully)
+
+# Returns 0 if they all succeeded, nonzero otherwise (1 if there was
+# an error when executing the notebook
+
+cd ../notebooks
+
+mkdir -p ../_notebook-output
+
+find . -name '*.ipynb' -depth 1 \
+     ! -name 'Postgres example.ipynb' \
+     -print0 |
+    xargs -0 -n 1 -I {} papermill "{}" "../_notebook-output/{}"
