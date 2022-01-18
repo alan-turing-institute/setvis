cea2bc3b4ecd900b1a407dc44a46f40f442b5300
Oliver Strickson <ostrickson@turing.ac.uk>
2022-01-18 10:38:37 +0000

    Exclude notebooks that need a database (for now)

--- tests/check_notebooks.sh
+++ tests/check_notebooks.sh
@@ -13,5 +13,7 @@ mkdir -p ../_notebook-output
 find . -maxdepth 1 \
      -name '*.ipynb' \
      ! -name 'Postgres example.ipynb' \
+     ! -name 'Tutorial 3 - Loading data from Postgres.ipynb' \
+     ! -name 'Tutorial 3 (supplemental) - Create the Postgres database.ipynb' \
      -print0 |
     xargs -0 -n 1 -I {} papermill "{}" "../_notebook-output/{}"
