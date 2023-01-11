c128695c858778bf0bb065302bc77374401dce0f
pwochner <pwochner@turing.ac.uk>
2023-01-11 15:10:27 +0000

    Fix import from plotutils in init.

--- pace/__init__.py
+++ pace/__init__.py
@@ -19,6 +19,15 @@ from .membership import (
     Col,
     Membership,
     selection_to_series,
+)
+from .history import (
+    Selection,
+    SubSelection,
+    SelectionHistory,
+    iterated_apply,
+    drop_selection,
+)
+from .plotutils import (
     intersection_bar_chart_data,
     intersection_cardinality_histogram_data,
     intersection_degree_histogram_data,
@@ -27,11 +36,4 @@ from .membership import (
     set_bar_chart_data,
     set_cardinality_histogram_data,
     intersection_heatmap_data,
-)
-from .history import (
-    Selection,
-    SubSelection,
-    SelectionHistory,
-    iterated_apply,
-    drop_selection,
 )
