e2c12c91fcdd02fcfc876e3fe0b17b253a2e7d96
Oliver Strickson <ostrickson@turing.ac.uk>
2023-02-26 15:44:25 +0000

    Remove nbsphinx dependency

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,6 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
-    "nbsphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
